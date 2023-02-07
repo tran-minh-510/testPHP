8efb99d56feee2df12aef15d934bf5ba95e467f7
MinhDuc0510 <minhduc53002@gmail.com>
2023-02-07 23:14:53 +0700

    7/2/2023

--- index.php
+++ index.php
@@ -2,49 +2,49 @@
 
 $arr = [
     [
-        'name'=>'Nguyễn Văn A',
-        'age'=>'18',
-        'email'=>'emailgiongnhau11111111@gmail.com',
-        'address'=>'Hà Nội'
+        'name' => 'Nguyễn Văn A',
+        'age' => '18',
+        'email' => 'emailgiongnhau11111111@gmail.com',
+        'address' => 'Hà Nội'
     ],
     [
-        'name'=>'Nguyễn Văn B',
-        'age'=>'19',
-        'email'=>'emailgiongnhau22222222@gmail.com',
-        'address'=>'Ninh Bình'
+        'name' => 'Nguyễn Văn B',
+        'age' => '19',
+        'email' => 'emailgiongnhau22222222@gmail.com',
+        'address' => 'Ninh Bình'
     ],
     [
-        'name'=>'Nguyễn Văn C',
-        'age'=>'20',
-        'email'=>'emailgiongnhau11111111@gmail.com',
-        'address'=>'Nam Định'
+        'name' => 'Nguyễn Văn C',
+        'age' => '20',
+        'email' => 'emailgiongnhau11111111@gmail.com',
+        'address' => 'Nam Định'
     ],
     [
-        'name'=>'Nguyễn Văn D',
-        'age'=>'22',
-        'email'=>'nguyenvand@gmail.com',
-        'address'=>'Hà Nội'
+        'name' => 'Nguyễn Văn D',
+        'age' => '22',
+        'email' => 'nguyenvand@gmail.com',
+        'address' => 'Hà Nội'
     ],
     [
-        'name'=>'Nguyễn Văn E',
-        'age'=>'28',
-        'email'=>'emailgiongnhau22222222@gmail.com',
-        'address'=>'Hà Nội'
+        'name' => 'Nguyễn Văn E',
+        'age' => '28',
+        'email' => 'emailgiongnhau22222222@gmail.com',
+        'address' => 'Hà Nội'
     ],
     [
-        'name'=>'Nguyễn Văn F',
-        'age'=>'38',
-        'email'=>'nguyenvanf@gmail.com',
-        'address'=>'Phú Thọ'
+        'name' => 'Nguyễn Văn F',
+        'age' => '38',
+        'email' => 'nguyenvanf@gmail.com',
+        'address' => 'Phú Thọ'
     ],
 ];
 
 $stringTable = '';
 
-foreach($arr as $index=>$arrs){
+foreach ($arr as $index => $arrs) {
     $index++;
     $infoTables = "<tr><td>$index</td>";
-    foreach($arrs as $i=>$arrsElement){
+    foreach ($arrs as $i => $arrsElement) {
         $infoTable = "<td>$arrs[$i]</td>";
         $infoTables .= $infoTable;
     }
@@ -69,34 +69,32 @@ $emailTrung = '';
 $mailDaTrung = [];
 $count = 0;
 
-foreach($arr as $index1=>$arr1){
-    foreach($arr as $index2=>$arr2){
-        if($index1!==$index2){
-            if($arr1['email']===$arr2['email']){
-                if(!in_array($arr1['email'], $mailDaTrung)){
+foreach ($arr as $index1 => $arr1) {
+    foreach ($arr as $index2 => $arr2) {
+        if ($index1 !== $index2) {
+            if ($arr1['email'] === $arr2['email']) {
+                if (!in_array($arr1['email'], $mailDaTrung)) {
 
-                                    $mailDaTrung[]=$arr1['email'];
-                                    $count++;
-                    $emailTrung .= "<tr><td>$count</td>";
-                    foreach($arr[$index1] as $keyChildArr1=>$childArr1){
-                        $stringInfo=$arr[$index1][$keyChildArr1];
-                        $emailTrung .= "<td>$stringInfo</td>";
-                    }
-                    $emailTrung .= '</tr>'; 
+                    $mailDaTrung[] = $arr1['email'];
                     $count++;
                     $emailTrung .= "<tr><td>$count</td>";
-                    foreach($arr[$index2] as $keyChildArr2=>$childArr2){
-                        $stringInfo=$arr[$index2][$keyChildArr2];
+                    foreach ($arr[$index1] as $keyChildArr1 => $childArr1) {
+                        $stringInfo = $arr[$index1][$keyChildArr1];
                         $emailTrung .= "<td>$stringInfo</td>";
                     }
                     $emailTrung .= '</tr>';
-                                }
+                    $count++;
+                    $emailTrung .= "<tr><td>$count</td>";
+                    foreach ($arr[$index2] as $keyChildArr2 => $childArr2) {
+                        $stringInfo = $arr[$index2][$keyChildArr2];
+                        $emailTrung .= "<td>$stringInfo</td>";
+                    }
+                    $emailTrung .= '</tr>';
                 }
+            }
         }
     }
 }
-
-// echo $emailTrung;
 
 echo "<table>
 <tr>
@@ -108,4 +106,3 @@ echo "<table>
 </tr>
 $emailTrung
 </table>";
-
